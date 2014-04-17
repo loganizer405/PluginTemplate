6d47ee51f5f09ea3028c2b414d1b880d094f981e
Logan Kramer <loganizer405@gmail.com>
2014-04-17 11:58:00 -0400

    Initial Commit

---
+++ PluginTemplate/Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PluginTemplate
+{
+    public class Config
+    {
+
+       //define public variables here
+
+        public void Write(string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public static Config Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return new Config();
+            }
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+        }
+    }
+}
---
+++ PluginTemplate/Main.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Newtonsoft.Json;
+
+
+namespace PluginTemplate
+{
+    [ApiVersion(1, 15)]
+    public class PluginTemplate : TerrariaPlugin
+    {
+        string path = Path.Combine(TShock.SavePath, "PluginTemplate.json");
+        Config Config = new Config();
+        public override string Name
+        {
+            get
+            {
+                return "PluginTemplate";
+            }
+        }
+        public override string Author
+        {
+            get
+            {
+                return "Your name here.";
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "A description of your plugin goes here.";
+            }
+        }
+        public override Version Version
+        {
+            get
+            {
+                return new Version("1.0");
+            }
+        }
+        public PluginTemplate(Main game)
+            : base(game)
+        {
+            Order = 1;
+        }
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+        public void OnInitialize(EventArgs args)
+        {
+            if (!File.Exists(path))
+            {
+                Config.Write(path);
+            }
+            Config = Config.Read(path);
+            //add commands here
+        }
+    }
+}
